d9250426631d3828516e3b6410c902db500236ee
Mark Niu <niuh@bc.edu>
2020-09-15 22:19:11 -0400

    Final

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 // TODO: load the dataset 
-
 let attractions;
-fetch('http://example.com/movies.https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9d7f28c1-b3cb-41cc-b1ab-c42f5b62beeb/attractions.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200915%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200915T232738Z&X-Amz-Expires=86400&X-Amz-Signature=249ec163c4bf7786f25217c68a520ecceab62a13015efd0d8d990217bd215674&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22attractions.json%22')
+
+fetch('./attractions.json')
   .then(response => response.json())
   .then(data => {
 		attractions = data;
-		console.log('movies - (a)', attractions);
+		filterData("all");
 	});
 
-
-console.log('movies - (b)', attractions);  
-
 function filterData(category) {
+	let dArray;
 
-	/* **************************************************
-	 *
-	 * TODO: filter attractions by the selected category
-	 * TODO: filter top 5 attractions
-	 *
-	 * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
-	 *
-	 * renderBarChart(data)
-	 *
-	 * - 'data' must be an array of JSON objects
-	 * - the max. length of 'data' is 5
-	 *
-	 * **************************************************/
+	if (category != "all") {
+		dArray = attractions.filter(a => a.Category === category);
+	}
+	else {
+		dArray = attractions;
+	}
 
+	dArray.sort((a, b) => parseFloat(b.Visitors) - parseFloat(a.Visitors));
+	slicedArray = dArray.slice(0, 5);
+	renderBarChart(slicedArray);
 }
 
-// TODO: Define an event listener for the dropdown menu
-//       Call filterData with the selected category
+let selectedEvent = document.querySelector("#attraction-category").addEventListener("change", (event)=>{
+	filterData(event.target.value);
+});
 
